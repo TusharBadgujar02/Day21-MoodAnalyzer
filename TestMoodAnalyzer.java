4602235702860bb0515a61d07d102b94254438f1
9685959802 <tusharbadgujar0000@gmail.com>
2022-10-10 22:08:50 +0530

    take good message

--- TestMoodAnalyzer.java
+++ TestMoodAnalyzer.java
@@ -1,24 +1,22 @@
 package moodanalyzer;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import demoMoodAnalyzer.MoodAnalyzer;
-
+import org.junit.Test;
 public class TestMoodAnalyzer {
-
 	@Test
 	public void givenMessage_whenSad_shouldReturnSad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String mood = moodAnalyzer.analyzeMood("This is a sad message");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+		String mood = moodAnalyzer.analyzeMood();
 		assertEquals("SAD", mood);
 	}
 	
 	@Test
 	public void givenMessage_whenNotSad_shouldReturnHappy() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String mood = moodAnalyzer.analyzeMood("This is a happy message");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+		String mood = moodAnalyzer.analyzeMood();
 		assertEquals("HAPPY", mood);
-	}
+	}	
 }
