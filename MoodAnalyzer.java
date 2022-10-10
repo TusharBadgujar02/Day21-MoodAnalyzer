e583baa607d0ce33aee683461af3fda894111af9
Tushar Badgujar <109934104+TusharBadgujar02@users.noreply.github.com>
2022-10-10 22:10:50 +0530

    take good message.

---
+++ MoodAnalyzer.java
@@ -0,0 +1,26 @@
+package demoMoodAnalyzer;
+
+public class MoodAnalyzer {
+private String message;
+	
+	public MoodAnalyzer() {
+	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyzeMood() throws MoodAnalysisException {
+		try {
+			if(message.length() == 0) {
+				throw new MoodAnalysisException(ExceptionType.ENTERED_EMPTY, "You entered an Invalid input!\\nPlease enter a non-empty input for mood.");
+			}
+				
+			if(message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException(ExceptionType.ENTERED_NULL, "You entered an Invalid input!\\nPlease enter a non-NULL input for mood.");
+		}
+	}	}
